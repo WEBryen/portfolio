9cc0a15f114b890f339bd184552237a0eec5af57
Aryan Singh <155964982+WEBryen@users.noreply.github.com>
2024-04-20 21:51:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+const cards = document.querySelectorAll(".tile");
+
+const setClasses = () => {
+  const classes = ["left", "active", "right"];
+  cards.forEach((card, index) => card.classList.add(classes[index]));
+};
+
+const changePositions = (e) => {
+  const clickedCard = e.currentTarget;
+  const activeCard = document.querySelector(".tile.active");
+  if (clickedCard.classList.contains("active")) return;
+  const classesFrom = e.currentTarget.className;
+  const classesTo = activeCard.className;
+  clickedCard.className = classesTo;
+  activeCard.className = classesFrom;
+};
+
+cards.forEach((card) => {
+  card.addEventListener("click", changePositions);
+});
+
+setClasses();
+
+
+
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
+
+
+
+function progressBarAndCountNumber () {
+    const progress = document.querySelectorAll('.progress');
+    let count = 0;
+    let MAX = 100;
+
+    let run = setInterval(() => {
+        count++;
+        progress.forEach(element => {
+            if (count <= element.dataset.progress) {
+                element.parentElement.style.background = `conic-gradient(#B46B8C ${count}%, #212428 0)`;
+                element.firstElementChild.textContent = `${count}%`;
+            };
+            if (count == MAX) {
+                clearInterval(run);
+            };
+        });
+    }, 20);
+}
+
+progressBarAndCountNumber();
